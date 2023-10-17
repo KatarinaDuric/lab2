87c3e31e24f47888a1b92b3edef302ad593d22cb
kd374 <kd374@cornell.edu>
2023-10-16 22:03:39 -0400

    jalr changes

--- lab2_proc/ProcBaseDpath.v
+++ lab2_proc/ProcBaseDpath.v
@@ -100,6 +100,9 @@ module lab2_proc_ProcBaseDpath
   logic [31:0] pc_plus4_F;
   logic [31:0] br_target_X;
   logic [31:0] jal_target_D;
+  logic [31:0] jalr_target_X;
+
+  assign jalr_target_X = alu_result_X & 32'hfffffffe;
 
   vc_EnResetReg#(32, c_reset_vector - 32'd4) pc_reg_F
   (
@@ -121,7 +124,7 @@ module lab2_proc_ProcBaseDpath
     .in0  (pc_plus4_F),
     .in1  (br_target_X),
     .in2  (jal_target_D),
-    .in3  (),
+    .in3  (jalr_target_X),
     .sel  (pc_sel_F),
     .out  (pc_next_F)
   );
